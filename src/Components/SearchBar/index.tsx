4d0cb4e65272aab101badf24a07d0c2332798cc8
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-11 02:12:10 +0900

    Feat: 검색바 디자인 구현

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -1,7 +1,45 @@
+import { Box, TextField, InputAdornment } from '@mui/material';
+import styled from 'styled-components';
+import SearchIcon from '@mui/icons-material/Search';
+
 export default function SearchBar() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Box sx={{ marginBottom: 12 }}>
+      <Search
+        fullWidth
+        sx={{
+          '& .MuiOutlinedInput-root': {
+            '& .MuiOutlinedInput-input': {
+              padding: '13px 0',
+              fontSize: 15,
+            },
+            '& fieldset': {
+              borderColor: 'rgb(201, 202, 204)',
+            },
+            '&:hover fieldset': {
+              borderColor: 'rgb(201, 202, 204)',
+            },
+            '&.Mui-focused fieldset': {
+              borderWidth: 1,
+              borderColor: '#524fa1',
+            },
+          },
+        }}
+        placeholder="배우고 싶은 언어, 기술을 검색해 보세요"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Box>
   );
 }
+
+const Search = styled(TextField)`
+  background-color: white;
+  border-radius: 4px;
+  border-color: rgb(201, 202, 204);
+`;
