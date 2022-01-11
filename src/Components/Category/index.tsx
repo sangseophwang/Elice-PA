a24c452bb32c2e949ac2fb07d43b65cb0dc672e1
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-11 15:57:51 +0900

    Feat: 카테고리 기본 디자인 구현

--- src/Components/Category/index.tsx
+++ src/Components/Category/index.tsx
@@ -1,7 +1,56 @@
+import {
+  Chip,
+  Table,
+  Paper,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableContainer,
+} from '@mui/material';
+import styled from 'styled-components';
+
+const LIST = ['유형', '진행 방식', '분야', '난이도', '언어', '가격'];
+
 export default function Category() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <TableContainer
+        component={Paper}
+        sx={{ boxShadow: 'none', borderRadius: 0, marginBottom: 3 }}
+      >
+        <Table>
+          <TableBody>
+            {LIST.map((element, index) => (
+              <Row key={index}>
+                <Title>{element}</Title>
+                <Data>
+                  <Chip label="hello" />
+                </Data>
+              </Row>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
+
+const Row = styled(TableRow)`
+  height: 18px;
+  border: 1px solid #e1e2e3;
+`;
+
+const Title = styled(TableCell)`
+  background-color: #f9fafc;
+  width: 64px;
+  font-size: 12px;
+  padding: 14px 16px;
+  font-weight: 700;
+  color: #5e5f61;
+  letter-spacing: 0.5px;
+  border-right: 1px solid #e0e0e0;
+`;
+
+const Data = styled(TableCell)`
+  padding: 8px 16px;
+`;
