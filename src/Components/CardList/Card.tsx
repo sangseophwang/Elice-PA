8a82cffefda0f11ecb2f26ac8bd08c52042a6a6e
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-12 15:57:44 +0900

    Feat: 카드리스트 컴포넌트 구현

--- src/Components/CardList/Card.tsx
+++
@@ -1,7 +0,0 @@
-export default function Card() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
-}
--- src/Components/CardList/index.tsx
+++ src/Components/CardList/index.tsx
@@ -1,7 +1,48 @@
-export default function CardList() {
+import { Typography, Divider, Grid, Card } from '@mui/material';
+import Header from 'Components/CardList/Header';
+import Content from 'Components/CardList/Content';
+import { DataInterface } from 'Pages/interface';
+import styled from 'styled-components';
+
+interface CardListProps {
+  course: number;
+  data: DataInterface[];
+}
+
+export default function CardList({ course, data }: CardListProps) {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <Typography sx={{ fontSize: 12, fontWeight: 700, marginBottom: 1 }}>
+        전체 {course}개
+      </Typography>
+      <Divider sx={{ marginBottom: '14px' }} />
+      <Grid container spacing={1.66}>
+        {data && data.length > 0 ? (
+          data.map((element, index) => (
+            <Grid item key={index}>
+              <CourseCard variant="outlined" sx={{ cursor: 'pointer' }}>
+                <Header
+                  title={element.title}
+                  free={element.is_free}
+                  type={element.enroll_type}
+                  description={element.short_description}
+                />
+                <Content url={element.logo_file_url} />
+              </CourseCard>
+            </Grid>
+          ))
+        ) : (
+          <div>
+            <h1>검색 결과가 없습니다.</h1>
+          </div>
+        )}
+      </Grid>
+    </>
   );
 }
+
+const CourseCard = styled(Card)`
+  padding: 28px 24px;
+  width: 248px;
+  height: 338px;
+`;
